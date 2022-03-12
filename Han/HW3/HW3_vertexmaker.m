fbcf151ad3f0de010168fc7c6d19aa45e3043a08
Seong-Min,Han <97667688+Hanseong-Min@users.noreply.github.com>
2022-03-12 22:10:00 +0900

    Update HW3_vertexmaker.m

--- Han/HW3/HW3_vertexmaker.m
+++ Han/HW3/HW3_vertexmaker.m
@@ -1,1 +1,22 @@
+%%% HW3_Vertex maker: use num circle
+%%% written by Seong-Min, Han
+N = 4; % 가중치
+numc = 4; % circle의 개수
+numv = 2*numc*numc+2*numc+1; % Vertex 개수
+Vertex = zeros(numv, 2);
 
+R = zeros(numc,1); % 각 원의 반지름
+for i=2:numc
+    R(1,1) = 3;
+    R(i,1) = R(1,1) * i;
+end
+
+for i=1:numc
+    for j=1:1:i*N
+        Vertex(2*(i*i-i+1)-1+j,1) = R(i,1) * cos((j-1)*pi/(2*i));
+        Vertex(2*(i*i-i+1)-1+j,2) = R(i,1) * sin((j-1)*pi/(2*i));
+    end
+end
+
+writematrix(Vertex,'Vertex.txt');
+type Vertex.txt
