f221bb0a09b357b53d1e3f06fff28ec0f6a71569
sangmok0224 <98933888+sangmok0224@users.noreply.github.com>
2022-03-14 21:04:18 +0900

    Update Homework3_visualize.m

--- Jeong/HW3/Homework3_visualize.m
+++ Jeong/HW3/Homework3_visualize.m
@@ -7,13 +7,13 @@ P = importdata("potential.txt");
 
 % Visualize 
 figure(1);
-patch('Faces',F,'Vertices',V, 'FaceVertexCData',P, 'EdgeColor','black','FaceColor','none','LineWidth',2);
-xlabel('X');
+patch('Faces', F , 'Vertices', V , 'EdgeColor','black','FaceColor','none','LineWidth',2);
+xlabel('X');https://github.com/hi2ska2/cm2022s/blob/main/Jeong/HW3/Homework3_visualize.m
 ylabel('Y');
 title('Structure')
 
 figure(2);
-patch('Faces',F,'Vertices',V, 'FaceVertexCData',P, 'EdgeColor','black','FaceColor','interp');
+patch('Faces', F ,'Vertices', V , 'FaceVertexCData', P , 'EdgeColor','black','FaceColor','interp');
 title('Structure (N=16)')
 xlabel('X');
 ylabel('Y');
